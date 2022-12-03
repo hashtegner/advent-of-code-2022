833bd311886f4475d9804e5d33d838b0075d3325
Alessandro.tegner <alessandro.tegner@gmail.com>
2022-12-03 20:49:25 -0300

    Second day

--- 2/main.go
+++ 2/main.go
@@ -16,6 +16,10 @@ type shape struct {
 	winsFrom string
 
 	points int
+
+	mustWin   bool
+	mustDrawn bool
+	mustLose  bool
 }
 
 func (s shape) isDrawn(b shape) bool {
@@ -26,26 +30,38 @@ func (s shape) win(b shape) bool {
 	return s.winsFrom == b.name
 }
 
-func (s shape) loses(b shape) bool {
-	return !s.isDrawn(b) && !s.win(b)
+func (s shape) determineMove(opponent shape) shape {
+	if s.mustWin {
+		return shapes[opponent.losesTo]
+	}
+
+	if s.mustLose {
+		return shapes[opponent.winsFrom]
+	}
+
+	return shapes[opponent.name]
 }
 
-func (s shape) roundPoints(b shape) int {
-	if s.isDrawn(b) {
+func (s shape) determineRoundPoints(opponent shape) int {
+	if s.isDrawn(opponent) {
 		return 3
 	}
 
-	if s.win(b) {
+	if s.win(opponent) {
 		return 6
 	}
 
 	return 0
 }
 
+func (s shape) roundPoints(opponent shape) int {
+	return s.points + s.determineRoundPoints(opponent)
+}
+
 var shapes = map[string]shape{
-	"rock":     {"rock", "X", "A", "paper", "scissors", 1},
-	"paper":    {"paper", "Y", "B", "scissors", "rock", 2},
-	"scissors": {"scissors", "Z", "C", "rock", "paper", 3},
+	"rock":     {"rock", "X", "A", "paper", "scissors", 1, false, false, true},
+	"paper":    {"paper", "Y", "B", "scissors", "rock", 2, false, true, false},
+	"scissors": {"scissors", "Z", "C", "rock", "paper", 3, true, false, false},
 }
 
 var meShapes = map[string]shape{
@@ -60,10 +76,6 @@ var opponentShapes = map[string]shape{
 	"C": shapes["scissors"],
 }
 
-func calculateScore(opponent, me shape) int {
-	return me.points + me.roundPoints(opponent)
-}
-
 func main() {
 	file, err := os.Open("./2/input.txt")
 	if err != nil {
@@ -74,12 +86,14 @@ func main() {
 	scanner := bufio.NewScanner(file)
 
 	score := 0
+	determined_score := 0
 	for scanner.Scan() {
 		text := scanner.Text()
 		opponent, me := text[0:1], text[2:3]
 		opponentShape, meShape := opponentShapes[opponent], meShapes[me]
+		score += meShape.roundPoints(opponentShape)
+		determined_score += meShape.determineMove(opponentShape).roundPoints(opponentShape)
 
-		score += calculateScore(opponentShape, meShape)
 	}
 
 	if err := scanner.Err(); err != nil {
@@ -87,4 +101,5 @@ func main() {
 	}
 
 	fmt.Println("Total score:", score)
+	fmt.Println("Determined Move Total score:", determined_score)
 }
--- 2/main_test.go
+++ 2/main_test.go
@@ -2,21 +2,50 @@ package main
 
 import "testing"
 
-func TestCalculateScore(t *testing.T) {
+func TestDetermineMove(t *testing.T) {
 	tests := []struct {
-		opponent string
-		me       string
-		expected int
+		opponent shape
+		me       shape
+
+		expected shape
 	}{
-		{"A", "Y", 8},
-		{"B", "X", 1},
-		{"C", "Z", 6},
+		{shapes["rock"], meShapes["Y"], shapes["rock"]},         // should return in draw
+		{shapes["paper"], meShapes["Y"], shapes["paper"]},       // should return in draw
+		{shapes["scissors"], meShapes["Y"], shapes["scissors"]}, // should return in draw
+
+		{shapes["paper"], meShapes["X"], shapes["rock"]},     // should return in lose
+		{shapes["rock"], meShapes["X"], shapes["scissors"]},  // should return in lose
+		{shapes["scissors"], meShapes["X"], shapes["paper"]}, // should return in lose
+
+		{shapes["scissors"], meShapes["Z"], shapes["rock"]},  // should return in win
+		{shapes["rock"], meShapes["Z"], shapes["paper"]},     // should return in win
+		{shapes["paper"], meShapes["Z"], shapes["scissors"]}, // should return in win
 	}
 
 	for _, test := range tests {
-		result := calculateScore(opponentShapes[test.opponent], meShapes[test.me])
+		result := test.me.determineMove(test.opponent)
+		if result.name != test.expected.name {
+			t.Fatalf("opponent %v, me %v should => result in %v, but is %v", test.opponent, test.me, test.expected, result)
+		}
+	}
+
+}
+
+func TestCalculateScore(t *testing.T) {
+	tests := []struct {
+		opponent shape
+		me       shape
+		expected int
+	}{
+		{opponentShapes["A"], meShapes["Y"], 8},
+		{opponentShapes["B"], meShapes["X"], 1},
+		{opponentShapes["C"], meShapes["Z"], 6},
+	}
+
+	for _, test := range tests {
+		result := test.me.roundPoints(test.opponent)
 		if result != test.expected {
-			t.Fatalf("%s%s should result in %v, but is %v", test.opponent, test.me, test.expected, result)
+			t.Fatalf("opponent %v, me %v should result in %v, but is %v", test.opponent, test.me, test.expected, result)
 		}
 	}
 }
